3229e99b734651fc1505c56dc8ec897b8b0586aa
luisadsfatec@hotmail.com <luisadsfatec@hotmail.com>
2023-09-19 23:41:57 -0300

    Atualizado plugin do joinchat

--- wp-content/plugins/creame-whatsapp-me/public/class-joinchat-public.php
+++ wp-content/plugins/creame-whatsapp-me/public/class-joinchat-public.php
@@ -130,29 +130,26 @@ class Joinchat_Public {
 
 		wp_enqueue_style( JOINCHAT_SLUG, plugins_url( "css/{$file}{$min}.css", __FILE__ ), array(), JOINCHAT_VERSION, 'all' );
 
-		if ( JOINCHAT_SLUG === $file ) {
-			$inline_css = '';
+		$inline_css = '';
 
-			if ( jc_common()->defaults( 'color' ) !== $settings['color'] ) {
-				list($r, $g, $b) = sscanf( $settings['color'], '#%02x%02x%02x' );
-				$inline_css     .= ".joinchat{ --red:$r; --green:$g; --blue:$b; }";
-			}
-
-			if ( ! empty( $settings['custom_css'] ) && jc_common()->defaults( 'custom_css' ) !== $settings['custom_css'] ) {
-				// Note that esc_html() cannot be used because `div &gt; span`.
-				$inline_css .= wp_strip_all_tags( $settings['custom_css'] );
-			}
-
-			$inline_css = apply_filters( 'joinchat_inline_style', $inline_css, $settings );
-
-			// Remove spaces & comments.
-			if ( ! defined( 'SCRIPT_DEBUG' ) || ! SCRIPT_DEBUG ) {
-				$inline_css = preg_replace( array( '/(\s*)([{|}|:|;|,])(\s+)/', '/\/\*.*?\*\/|\n|\t/' ), array( '$2', '' ), $inline_css );
-			}
-
-			wp_add_inline_style( JOINCHAT_SLUG, $inline_css );
+		if ( jc_common()->defaults( 'color' ) !== $settings['color'] ) {
+			list($r, $g, $b) = sscanf( $settings['color'], '#%02x%02x%02x' );
+			$inline_css     .= ".joinchat{ --red:$r; --green:$g; --blue:$b; }";
 		}
 
+		if ( ! empty( $settings['custom_css'] ) && jc_common()->defaults( 'custom_css' ) !== $settings['custom_css'] ) {
+			// Note that esc_html() cannot be used because `div &gt; span`.
+			$inline_css .= wp_strip_all_tags( $settings['custom_css'] );
+		}
+
+		$inline_css = apply_filters( 'joinchat_inline_style', $inline_css, $settings );
+
+		// Remove spaces & comments.
+		if ( ! defined( 'SCRIPT_DEBUG' ) || ! SCRIPT_DEBUG ) {
+			$inline_css = preg_replace( array( '/(\s*)([{|}|:|;|,])(\s+)/', '/\/\*.*?\*\/|\n|\t/' ), array( '$2', '' ), $inline_css );
+		}
+
+		wp_add_inline_style( JOINCHAT_SLUG, $inline_css );
 	}
 
 	/**
@@ -168,7 +165,7 @@ class Joinchat_Public {
 	public function enqueue_scripts() {
 
 		$min  = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
-		$deps = array( 'jquery' );
+		$deps = array( 'jquery-core' ); // Don't need jquery-migrate.
 
 		// Register QR script.
 		wp_register_script( 'joinchat-qr', plugins_url( 'js/qr-creator.min.js', __FILE__ ), array(), '1.0.0', true );
